eedb6708c7153d205a364d911c79fe8246a306c8
Benjamin Howes <91109173+capnganj@users.noreply.github.com>
2022-02-07 23:36:09 -0500

    hash smoke clouds - first real rips

--- src/index.js
+++ src/index.js
@@ -9,32 +9,12 @@ import { interpolateYlGn } from 'd3-scale-chromatic';
 //p5 sketch instance
 const s = ( sk ) => {
 
-  //global sketch variable
-  let seed = 0; //seed Hash
-  let rects = [];
+  //global sketch variables
+  let cloud = [];
 
   //sketch setup
   sk.setup = () => {
     sk.createCanvas(sk.windowWidth, sk.windowHeight);
-  
-    //set the p5 random seed by calling the fxrand function
-    seed=sk.int(fxrand() * 100000000); // FXHASH seed rand
-    sk.randomSeed(seed); 
-
-    //populate array of objects for rect size
-    for (let i = 0; i < 5; i++) {
-
-      //create color and set alpha
-      const c = sk.color(interpolateYlGn(fxrand()));
-      c.setAlpha(sk.map(fxrand(), 0, 1, 25, 150));
-
-      //properties to construct rects below
-      const element = {
-        "size": sk.int(sk.random(sk.width/2)),
-        "color": c
-      };
-      rects.push(element);
-    }
 
     //new featuresClass
     let feet = new HashSmokeFeatures();
@@ -48,7 +28,11 @@ const s = ( sk ) => {
       "Squint": feet.squint.tag,
       "Laugh" : feet.laugh.tag
     };
-    console.log(window.$fxhashFeatures);
+
+    //Generate cloud data
+    drawCloud(sk.width/2, sk.height/2, 600, 7);
+    console.log("fxhashFeatures", window.$fxhashFeatures);
+    console.log("cloudData", cloud);
   };
 
 
@@ -58,14 +42,39 @@ const s = ( sk ) => {
     //TO DO - set the background color.  This should be a desaturated inverse color average of the colors in the d3 color scheme
     sk.background(255);
     sk.noStroke();
-    sk.rectMode(sk.CENTER);
+    
 
     //TO DO - draw a radial pattern of circles using 
-    rects.forEach(element => {
+    cloud.forEach(element => {
+      sk.ellipseMode(sk.CENTER);
       sk.fill(element.color);
-      sk.rect(sk.width/2,sk.height/2,element.size,element.size);
+      sk.ellipse(element.x, element.y, element.size, element.size);
     });
   };
+
+  //recursive smoke cloud
+  function drawCloud( x, y, rad, num){
+    const c = sk.color(interpolateYlGn(fxrand()));
+    c.setAlpha(sk.map(fxrand(), 0, 1, 50, 255/num));
+    cloud.push(
+      {
+        'color': c,
+        'size' : rad * 2,
+        'x' : x,
+        'y' : y
+      }
+    );
+    if( num > 1 ){
+      num = num - 1;
+      let branch = sk.map(fxrand(), 0, 1, 2, 6);
+      for (let i = 0; i < branch; i++) {
+        let a = sk.map(fxrand(), 0, 1, 0, sk.TWO_PI);
+        let newX = x + Math.cos(a) * 50 * num;
+        let newY = y + Math.sin(a) * 50 * num;
+        drawCloud(newX, newY, sk.map(fxrand(),0,1,rad/1.5,rad/3), num);
+      }
+    }
+  }
 
   //handle window resize
   sk.windowResized = () => {
